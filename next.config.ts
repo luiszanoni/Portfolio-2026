439384a7dbd72610f6aaa59aa3fc70b1867da748
luisgustavo <luiszanoni01@gmail.com>
2026-02-16 17:54:23 -0300

    Remove basepath

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'Portfolio-2026';
+const repoName = '';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repoName}` : undefined,
-  assetPrefix: isProd ? `/${repoName}/` : undefined,
+
 };
 
 export default nextConfig;
